36987d00f761a4a5f75cecbadf1a94f5befbf70f
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-06-16 11:47:03 +0530

    [Harsh Upadhyay].Add Practice Problems For JavaFundamentals on Day2

---
+++ PraticeProblems/Day1_Question1_Assisted_Problems_Bridgelabz.java.java
@@ -0,0 +1,9 @@
+//1.Ans.
+class Day1_Question1_Assisted_Problems_Bridgelabz{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabzs");
+     
+    }
+}
+
+
