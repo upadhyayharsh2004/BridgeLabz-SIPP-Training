26980688ff250c9bf4b81117aaf3983a8218e4e5
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-06-16 11:38:44 +0530

    Revert "[Harsh Upadhyay].add Pratice Problems For JavaFundamentals on Day2"
    This reverts commit 75b578ecdb8ec8f69e8b7235a2b087610de3c556.

--- PraticeProblems/Day1_Question5_Self_Problems_Bridgelabz.java.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Day1_Question5_Self_Problems_Bridgelabz{
-    public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    double distance=sc.nextDouble();
-    double miles=distance*0.621371;
-    System.out.print(miles);
-    }
-}
