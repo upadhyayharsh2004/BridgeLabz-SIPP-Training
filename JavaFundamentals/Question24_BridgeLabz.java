36e9ab892182b306a8d862cf37335e55fdbf9f65
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-06-24 10:26:19 +0530

    Initial empty commit

--- JavaFundamentals/Question24_BridgeLabz.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Question24{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        float fahrenheit=sc.nextFloat();
-        float celsiusResult=(float)((fahrenheit-32)*1.8);
-        System.out.println("The"+" "+fahrenheit+" "+"fahrenheit is"+" "+Math.ceil(celsiusResult)+" "+"celsius");
-    }
-}
--- PraticeProblems/Day1_Question3_Assisted_Probelms_Bridgelabz.java.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Day1_Question3_Assisted_Probelms_Bridgelabz{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        double temperature=sc.nextDouble();
-        double fahrenheit=(temperature*1.8)+32;
-        System.out.print(fahrenheit);
-    }
-}
